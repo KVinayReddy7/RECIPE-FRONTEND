afa89553fd2ffb3e331eb211931b76fee539812b
Hemu2503 <118917390+Hemu2503@users.noreply.github.com>
2024-06-29 18:20:25 +0530

    helper.js
    updated baseurl in helper 2nd time

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,3 +1,4 @@
 // export const BASE_URL = "http://localhost:5500"
 // export const BASE_URL = "https://recipefinderbackend.onrender.com"
-export const BASE_URL = "https://recipe-backend-w8ia.onrender.com"
+// export const BASE_URL = "https://recipe-backend-w8ia.onrender.com"
+export const BASE_URL = "https://recipe-backend-ezn6.onrender.com"
